9126594af07c0cbc93a6c7f1caa07897bf2ba7e7
Sunil Thapa <sunilthapa056@outlook.com>
2025-08-04 13:26:00 +0545

    started with views

--- src/main/java/com/summer/quiz/controller/ViewController.java
+++ src/main/java/com/summer/quiz/controller/ViewController.java
@@ -1,4 +1,12 @@
 package com.summer.quiz.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ViewController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 }
